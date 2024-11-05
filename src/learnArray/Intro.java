97eeb820300c803b1e9c0df169b07ffa59fac4c3
durshettibalaji <durshettibalaji1@gmail.com>
2024-11-05 23:25:27 +0530

    testing

--- src/learnArray/Intro.java
+++ src/learnArray/Intro.java
@@ -1,7 +1,13 @@
 package learnArray;
 
 public class Intro {
-public static void main(String args[]){
+public static void main(String[] args){
+    int[] array = new int[5];
+    for(int i=0;i<array.length;i++)
+    {
+        array[i]=i;
+        System.out.println(array[i]);
+    }
     System.out.println("array is working");
 }
 }
