f8caebe5e29f62b197ca03384ae937875366515a
durshettibalaji <durshettibalaji1@gmail.com>
2024-11-12 23:10:51 +0530

    made changes

--- src/learnLinkedList/MiddleOfLinkedList.java
+++ src/learnLinkedList/MiddleOfLinkedList.java
@@ -1,13 +1,13 @@
 package learnLinkedList;
 
 public class MiddleOfLinkedList {
-    public static void main(String args[]){
+    public static void main(String[] args){
         int[] arr={10,20,30,40,50,60,70,80,90};
         Node head=new Node(1);
         Node cur =head;
-        for(int i=0;i<arr.length;i++) {
-            cur.next = new Node(arr[i]);
-            cur=cur.next;
+        for (int j : arr) {
+            cur.next = new Node(j);
+            cur = cur.next;
         }
         head.print(head);
         Node middle = middle(head);
